9854859bed095311b3fc13de16c40f68cd6f9e70
Anko <anna.jedrzejko@op.pl>
2021-05-10 15:02:55 +0200

    feat: Add loading state, update filter condition

--- src/components/RecipeListPage/RecipeListPage.js
+++ src/components/RecipeListPage/RecipeListPage.js
@@ -1,20 +1,22 @@
-
 import React, { useContext } from 'react';
 
 import RecipeCard from '../RecipeCard/RecipeCard';
 
 import { StoreContext } from '../../store/StoreProvider';
 
-const RecipeListPage = ({ match }) => {
-    const { recipes } = useContext(StoreContext);
 
-    let selectedRecipeIndex = []
+const RecipeListPage = ({ match }) => {
+
+    const { isLoading, recipes } = useContext(StoreContext);
+
+    let selectedRecipeIndex = [];
 
     recipes.forEach((recipe, index) => {
-        const selectedTags = recipe.tags.filter(tag => tag.name === match.params.name && tag.active === true)
 
-        if (selectedTags.length) selectedRecipeIndex.push(index)
-    })
+        const selectedTags = recipe.tags.filter(tag => tag.name === match.params.name)
+
+        if (Boolean(selectedTags.length)) selectedRecipeIndex.push(index)
+    });
 
     const selectedRecipesCards = recipes
         .filter(recipe => {
@@ -23,11 +25,12 @@ const RecipeListPage = ({ match }) => {
             }
             return false
         })
-        .map(recipe => <RecipeCard key={recipe.id} {...recipe} />)
+        .map(recipe => <RecipeCard key={recipe.id} {...recipe} />);
 
     return (
         <section className="container">
-            <div className="row row-cols-1 row-cols-md-4">
+            {isLoading && <p>Ładuję przepisy...</p>}
+            <div className="row row-cols-1 row-cols-md-4 gy-4">
                 {selectedRecipesCards}
             </div>
         </section>
