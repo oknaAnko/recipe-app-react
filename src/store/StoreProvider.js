02c95462f13f30417ff99933cfbfff00e9f7fbd4
Anko <anna.jedrzejko@op.pl>
2021-05-10 14:36:39 +0200

    feat: Fetch data with loading and error

--- src/store/StoreProvider.js
+++ src/store/StoreProvider.js
@@ -1,31 +1,52 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState, useRef } from 'react';
+
 import request from '../helpers/request';
 
 
 export const StoreContext = createContext(null);
+
 
 const StoreProvider = ({ children }) => {
     const [recipes, setRecipes] = useState([]);
     const [allTags, setAllTags] = useState([]);
     const [user, setUser] = useState(null);
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [recipeError, setRecipeError] = useState({});
+
+    const componentMounted = useRef(false);
+
     const getAllRecipes = async () => {
-        const { data } = await request.get('/recipes');
-        setRecipes(data)
+        setIsLoading(true);
+        try {
+            setRecipeError({})
+            const { data } = await request.get('/recipes');
+            if (componentMounted.current) setRecipes(data)
+        } catch (err) {
+            setRecipeError(err)
+        }
+        setIsLoading(false);
     };
 
     const getAllTags = async () => {
-        const { data } = await request.get('/tags');
-        setAllTags(data)
-    }
+        try {
+            setRecipeError({})
+            const { data } = await request.get('/tags');
+            if (componentMounted.current) setAllTags(data)
+        } catch (err) {
+            setRecipeError(err)
+        }
+    };
 
     useEffect(() => {
+        componentMounted.current = true;
         getAllRecipes();
         getAllTags();
-    }, [])
+        return () => { componentMounted.current = false }
+    }, []);
 
     return (
-        <StoreContext.Provider value={{ recipes, setRecipes, allTags, user, setUser }}>
+        <StoreContext.Provider value={{ isLoading, recipeError, recipes, setRecipes, allTags, user, setUser }}>
             {children}
         </StoreContext.Provider>
     );
