046c853dd0ed37f8ed215fac706b76fb05b233fc
Anko <anna.jedrzejko@op.pl>
2022-01-29 23:07:46 +0100

    feat: Remove StoreProvider and unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux'; 
+import { Provider } from 'react-redux';
 import store from './store/store';
 
 import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header.tsx';
 
-import './App.scss'  
+import './App.scss';
 
-import StoreProvider from './store/StoreProvider';
-
-const App = () => { 
+const App = () => {
   return (
-    <Provider store={store}> 
-      <StoreProvider>
-        <Router>
-          <Header />
-          <Content />
-        </Router>
-        <Footer />
-        </StoreProvider>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Content />
+      </Router>
+      <Footer />
     </Provider>
   );
-}
+};
 
 export default App;
