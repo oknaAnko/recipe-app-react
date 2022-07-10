c3298d539b9c43fd02dc2dec5396dea4fa52a69e
Anko <anna.jedrzejko@op.pl>
2022-07-10 20:51:37 +0200

    feat: Create footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer className="border-top bg-light shadow-sm text-center">
-            <div className="max-width mx-auto">
-                Footer
-            </div>
-        </footer>
-    );
-}
+  return (
+    <footer className='border-top bg-light shadow-lg  text-center'>
+      <div className='max-width mx-auto'>okna_Anko &copy; {new Date().getFullYear()} All Rights Reserved</div>
+    </footer>
+  );
+};
 
 export default Footer;
